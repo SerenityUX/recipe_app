c52cbba8db7437810a4cdf38786da5b803a89c24
Thomas Stubblefield <tstubblefield487@gmail.com>
2022-03-14 18:34:10 -0400

    finally fixed the server issues

--- pages/recipe_page/[recipe].js
+++ pages/recipe_page/[recipe].js
@@ -19,24 +19,24 @@ export default function Recipe (props) {
         <div>
         <div className={styles.top_bar}>
             <a href=" / "><img src="https://i.ibb.co/SVPT9Yn/backbutton.png" alt=""/></a>
-            <p>{list_of_recipes[recipe - 1].name}</p>
+            <p>{list_of_recipes[recipe - 1]?.name}</p>
         </div>
-        <img src={list_of_recipes[recipe - 1].thumbnail} className={styles.thumbnail} alt=""/>
-        <h1  className={styles.title}>{list_of_recipes[recipe - 1].name}</h1>
+        <img src={list_of_recipes[recipe - 1]?.thumbnail} className={styles.thumbnail} alt=""/>
+        <h1  className={styles.title}>{list_of_recipes[recipe - 1]?.name}</h1>
         <div className={styles.author}>
-            <img src={list_of_recipes[recipe - 1].author.avatar} alt=""/>
-            <p>{list_of_recipes[recipe - 1].author.name}</p>
+            <img src={list_of_recipes[recipe - 1]?.author.avatar} alt=""/>
+            <p>{list_of_recipes[recipe - 1]?.author.name}</p>
         </div>
-        <p className={styles.description}>{list_of_recipes[recipe - 1].description}</p>
+        <p className={styles.description}>{list_of_recipes[recipe - 1]?.description}</p>
         <h2 className={styles.section_title}>Ingredients</h2>
-        {list_of_recipes[recipe - 1].ingredients.map(item => {
+        {list_of_recipes[recipe - 1]?.ingredients.map(item => {
             return(
                 <li key={item} className={styles.recipe_ingredients}>{item}</li>
               )
         })}
         <h2 className={styles.section_title}>Directions</h2>
         <ol type="1.">
-        {list_of_recipes[recipe - 1].directions.map(item => {
+        {list_of_recipes[recipe - 1]?.directions.map(item => {
             return(
                 <li key={item} className={styles.recipe_directions}>{item}</li>
               )
