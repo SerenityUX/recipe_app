f486e1e890c47c82f6168fe01f22a5dd0ebeb790
Thomas Stubblefield <tstubblefield487@gmail.com>
2022-03-27 00:09:22 -0400

    potential fix

--- pages/recipe_page/[id].js
+++ pages/recipe_page/[id].js
@@ -4,9 +4,10 @@ import { useRouter } from "next/router"
 import styles from '../../styles/recipeview.module.css'
 import Image from 'next/image'
 
-export async function getStaticProps(props) {
+export async function getStaticProps(context) {
     const { id } = context.params;
     console.log(id)
+
     const user_response = await fetch('https://dev.createforever.media/api:lSOVAmsS/users')
     const user_list = await user_response.json()
    
