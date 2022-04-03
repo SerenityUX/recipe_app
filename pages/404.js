ae56b8f4b642ac06b398d5ea9a8856de367020df
Thomas Stubblefield <tstubblefield487@gmail.com>
2022-04-02 23:44:35 -0400

    different error screen

--- pages/404.js
+++ pages/404.js
@@ -1,19 +1,16 @@
-import Error from 'next/error'
-
-export async function getServerSideProps() {
-  const res = await fetch('https://api.github.com/repos/vercel/next.js')
-  const errorCode = res.ok ? false : res.statusCode
-  const json = await res.json()
-
-  return {
-    props: { errorCode, stars: json.stargazers_count },
+function Error({ statusCode }) {
+    return (
+      <p>
+        {statusCode
+          ? `An error ${statusCode} occurred on server`
+          : 'An error occurred on client'}
+      </p>
+    )
   }
-}
-
-export default function Page({ errorCode, stars }) {
-  if (errorCode) {
-    return <Error statusCode={errorCode} />
+  
+  Error.getInitialProps = ({ res, err }) => {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+    return { statusCode }
   }
-
-  return <div>Next stars: {stars}</div>
-}
+  
+  export default Error
