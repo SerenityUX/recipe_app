7c0066ea34178affe06b8f52d022005306ee46da
Thomas Stubblefield <tstubblefield487@gmail.com>
2022-04-21 16:41:07 -0400

    layout made responsive

--- components/recipepreview.js
+++ components/recipepreview.js
@@ -33,7 +33,7 @@ export default function recipepreview (props) {
             </div>
             <div className={styles.right}>
                 <div  className={styles.thumbnail}>
-                <Image src={props.thumbnail} alt="" className={styles.thumbnailcontent} height={112} width={83} />
+                <Image layout="responsive" src={props.thumbnail} alt="" className={styles.thumbnailcontent} height={112} width={83} />
                 </div>
             </div>
             </a>
